b22ecbb387f9fb09abb070d13095a1a7a81f730e
Jose L Saboya <saboyadev@gmail.com>
2023-02-12 21:47:20 -0500

    Added eslint config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
-import './index.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 )
